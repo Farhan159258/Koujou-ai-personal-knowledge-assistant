683a61154d285103366454c276f4b50eaa703b5f
Farhan <farhanhayath@gmail.com>
2026-06-16 14:39:57 +0530

    Day 2: File upload feature completed

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,17 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, UploadFile, File
 import os
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 UPLOAD_FOLDER = "uploads"
 
